9ac87d3a30934a99f30f95de4cc928a270a52f9a
aggelostsiomitas <aggelostsiomitas@gmail.com>
2026-04-24 20:08:28 +0300

    Update vertexshader.glsl

--- res/Shaders/vertexshader.glsl
+++ res/Shaders/vertexshader.glsl
@@ -2,11 +2,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-uniform vec2 offset; // Η μεταβλητή για την κίνηση [cite: 27]
+uniform vec2 offset; 
 out vec3 ourColor;
 
 void main() {
-    // Εφαρμογή μετατόπισης [cite: 27]
+    // Εφαρμογή μετατόπισης 
     gl_Position = vec4(aPos.x + offset.x, aPos.y + offset.y, aPos.z, 1.0);
     ourColor = aColor;
 }
